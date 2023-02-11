731a4b075e22ccbe72c02032738aea8c47c85886
Swatantra Jain <swatantraj2002@gmail.com>
2023-02-11 23:11:45 +0800

    Time: 0 ms (100.00%), Space: 9.1 MB (75.16%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int ptr1 = m-1, ptr2 = n-1;
+        int k = m+n-1;
+        while(ptr1 >=0 && ptr2 >= 0){
+            if(nums1[ptr1] <= nums2[ptr2]){
+                nums1[k] = nums2[ptr2];
+                ptr2--;
+                k--;                
+            }
+            else{
+                nums1[k] = nums1[ptr1];
+                ptr1--;
+                k--;
+            }
+        }
+        while(ptr1>=0)
+            nums1[k--] = nums1[ptr1--];
+        
+        while(ptr2>=0)
+            nums1[k--] = nums2[ptr2--];
+        
+    }
+};
